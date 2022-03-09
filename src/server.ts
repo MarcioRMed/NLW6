7cd0faa59e528dd9ae22d0b81aaf102e502e0be2
MarcioRMed <contato.microinfo@gmail.com>
2022-03-09 05:09:50 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,11 @@
+import "reflect-metadata";
 import express from "express";
 
-//@types/express
+import "./database";
+
 const app = express();
 
-//Rotas
 
-/**
- * GET    => Busca uma informação
- * POST   => Inserir (criar) uma informação
- * PUT    => Altera uma informação
- * DELETE => Remover um dado
- * PATCH  => Altera uma informação específica
- */
-
-/**
- * Tipos de parâmetros
- * Router Params => http://localhost:3000/produtos/7874512136516
- * Query Params => http://localhost:3000/produtos?name=teclado&description=tecladobom&
- * 
- * Body Params =>{              //usado POST, PUT, PATCH
- * "name": "teclado",
- * "description": "teclado bom"
- * }
- */
-
-app.get("/test", (request, response) => {
-  
-
-  //Request => Entrando
-  //Response => Saindo
-  return response.send("Olá NLW metódo GET");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Ola NLW metódo POST");
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
 
 
